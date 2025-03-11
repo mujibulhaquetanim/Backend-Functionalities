c657e684a426ae32d166b48fcd7aab7cd50dba8f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-11 22:45:36 +0600

    show the relationship among models using findmany query

--- Databases/ORMs/Prisma/src/index.ts
+++ Databases/ORMs/Prisma/src/index.ts
@@ -26,17 +26,44 @@ async function main(){
     // })
 
     //method 2 for checking existing user and creating new user
-    const user = await prisma.user.upsert({
-        where: {email: "mujibai@me.com"},
-        update: {},
-        create: {
-            name: "mujibai",
-            email: "mujibai@me.com",
-            password: "baigan",
-            joinedAt: new Date(),
-        }
-    })
-    console.log('user: ',user);
+    // const user = await prisma.user.upsert({
+    //     where: {email: "mujibai@me.com"},
+    //     update: {},
+    //     create: {
+    //         name: "mujibai",
+    //         email: "mujibai@me.com",
+    //         password: "baigan",
+    //         joinedAt: new Date(),
+    //     }
+    // })
+    // console.log('user: ',user);
+
+    // const usersWithFriendships = await prisma.user.findMany({
+    //     include: {
+    //       friendships: {
+    //         include: {
+    //           user: true,
+    //         },
+    //       },
+    //     },
+    //   });
+
+    const usersWithDetails = await prisma.user.findMany({
+        include: {
+          friendships: {
+            include: {
+              user: true, // Include friend details
+            },
+          },
+          jobTitle: true, // Include job title details
+          profile: true, // Include profile details
+          posts: true, // Include posts details
+        },
+      });
+    
+      console.dir(usersWithDetails, { depth: null });
+    
+    //   console.dir(usersWithFriendships, { depth: null });
 
 }
 
