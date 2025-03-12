673deb11a3065bd9b1b309e7ff021047dff8bcf1
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-12 13:48:35 +0600

    `fix the import error` in the schema file and add exact error fix comment in it for future ref.

--- Databases/ORMs/Drizzle/src/db/schema/schema.ts
+++ Databases/ORMs/Drizzle/src/db/schema/schema.ts
@@ -1,6 +1,17 @@
-import * as users from './users.schema';
-import * as profileInfo from './profileInfo.schema';
-import * as posts from './posts.schema';
-import * as comments from './comments.schema';
-import * as groups from './groups.schema';
-export {users, profileInfo, posts, comments, groups};
+import { users } from "./users.schema";
+import { profileInfo } from "./profileInfo.schema";
+import { posts } from "./posts.schema";
+import { comments } from "./comments.schema";
+import { groups } from "./groups.schema";
+
+export { users, profileInfo, posts, comments, groups };
+
+/*
+import * as users from './users.schema'; 
+export { users };
+Here, users is not the actual PgTable instance but an object containing the module's exports. That's why Drizzle complains when you try to pass it to database.insert().
+
+
+error: Argument of type 'typeof import("path/to/schema")' is not assignable to parameter of type 'PgTable<TableConfig>'.
+suggests that the way you're exporting schemas from the index.ts file is incorrect. Instead of exporting individual schema objects, you are exporting modules (i.e., objects that contain all exports from each schema file). When you then try to use schema.users, it's referring to an entire module, not a PgTable<TableConfig>.
+*/
