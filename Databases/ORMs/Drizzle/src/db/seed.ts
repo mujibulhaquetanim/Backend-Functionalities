ca13010c0a32e7ab594852c1f9873141f7dc8b07
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-12 14:29:55 +0600

    make seed for post and comment schema

--- Databases/ORMs/Drizzle/src/db/seed.ts
+++ Databases/ORMs/Drizzle/src/db/seed.ts
@@ -3,7 +3,8 @@ import { database } from "./db";
 import * as schema from "./schema/schema";
 
 async function main() {
-    const users = await Promise.all(
+    //userId will contain returned id of each inserted user which is 50 times.
+    const userIds = await Promise.all(
         Array(50).fill("").map(async () => {
             const user = await database.insert(schema.users).values({
                 email: faker.internet.email(),
@@ -14,6 +15,29 @@ async function main() {
             return user[0].id; // return the id of the inserted user
         })
     );
+
+    const postIds = await Promise.all(
+        Array(50).fill("").map(async ()=>{
+            const post= await database.insert(schema.posts).values({
+                content: faker.lorem.paragraph(),
+                title: faker.lorem.sentence(),
+                authorId: faker.helpers.arrayElement(userIds) //faker.helpers will randomly select one of the userIds.
+            }).returning();
+            return post[0].id
+        })
+    )
+
+    //didn't use any variable name as comment won't be used in any of the created schemas.
+    await Promise.all(
+        Array(50).fill("").map(async ()=>{
+            const comment = await database.insert(schema.comments).values({
+                text: faker.lorem.paragraph(),
+                authorId: faker.helpers.arrayElement(userIds),
+                postId: faker.helpers.arrayElement(postIds)
+            }).returning();
+            return comment[0].id // it is not required though as it is not been using any of the schemas.
+        })
+    )
 }
 
 main().catch(console.error);
