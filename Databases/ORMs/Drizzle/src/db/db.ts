496bb3483a5e9ea029663bbdc2b14367baf976dd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-15 12:18:04 +0600

    `[ECONNEREFUSED BUG FIXED]:` load dotenv to ensure env variables are not undefined + add some customization in config file.

--- Databases/ORMs/Drizzle/src/db/db.ts
+++ Databases/ORMs/Drizzle/src/db/db.ts
@@ -1,6 +1,17 @@
+import dotenv from "dotenv";
 import postgres from "postgres";
-import {drizzle} from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/postgres-js";
 
-export const connection = postgres(process.env.DATABASE_URL!);
+//If .env is not loaded, process.env.DATABASE_URL will be undefined, and Drizzle will default to localhost:5432, causing ECONNREFUSED.
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set. Make sure your .env file is loaded.");
+}
+
+export const connection = postgres(process.env.DATABASE_URL, {
+  // This is the default, but I like to be explicit. it is a security protection against SQL injection attacks
+  ssl: 'require'
+});
 
 export const database = drizzle(connection);
