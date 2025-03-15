9b4f93d8aafaee83b4dff7f93d6b6e2617495179
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-15 12:25:23 +0600

    use `select` query to see the inserted data of users and move migrate file to `src` dir

--- Databases/ORMs/Drizzle/src/migrate.ts
+++ Databases/ORMs/Drizzle/src/migrate.ts
@@ -1,5 +1,5 @@
 import {migrate} from 'drizzle-orm/postgres-js/migrator';
-import {connection, database} from './src/db/db';
+import {connection, database} from './db/db';
 
 (async()=>{
     await migrate(database, {migrationsFolder: './drizzle'})
