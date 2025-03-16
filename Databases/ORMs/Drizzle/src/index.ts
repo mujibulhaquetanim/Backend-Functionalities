2801e077629ace9a22d4ba57008ad0f5acc6b714
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-16 23:54:22 +0600

    apply `update` query using drizzle

--- Databases/ORMs/Drizzle/src/index.ts
+++ Databases/ORMs/Drizzle/src/index.ts
@@ -1,10 +1,12 @@
+import { eq } from "drizzle-orm"
 import { database } from "./db/db"
+import { posts } from "./db/schema/posts.schema"
 // import * as schema from "./db/schema/schema"
 
 // const users = database.select().from(schema.users)
 const post = database.query.posts.findMany({
     // find authorId equal to 17
-    where: (posts, {eq})=> eq(posts.authorId, 17),
+    where: (posts, {eq})=> eq(posts.authorId, 48),
     limit: 2,
     with: {
         author: {
@@ -18,5 +20,11 @@ const post = database.query.posts.findMany({
         }
     }
 })
+
+const updatePost = database.update(posts).set({
+    title: "Updated Title"
+}).where(eq(posts.id, 48))
+
+updatePost.then(data => console.log(data)).catch(error => console.error(error));
 
 post.then(data => console.log(data)).catch(error => console.error(error));
