133e89db2a301e746ea264aef57cb3920e39b589
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-16 23:02:14 +0600

    `notNull` to maintain data integrity

--- Databases/ORMs/Drizzle/src/db/schema/posts.schema.ts
+++ Databases/ORMs/Drizzle/src/db/schema/posts.schema.ts
@@ -3,11 +3,12 @@ import { users } from "./users.schema";
 import { relations } from "drizzle-orm";
 import { comments } from "./comments.schema";
 
+//used notNull to maintain data integrity and ensure reliable application behavior. for example: If authorId is NULL, it means the post may not have an associated author. This could result in "orphaned" posts with no clear ownership.
 export const posts = pgTable('posts',{
     id: serial("id").primaryKey(),
-    title: text("title"),
-    content: text("content"),
-    authorId: integer("authorId").references(()=> users.id)
+    title: text("title").notNull(),
+    content: text("content").notNull(),
+    authorId: integer("authorId").notNull().references(()=> users.id, {onDelete: 'cascade'})
 })
 
 export const postRelations= relations(posts, ({one, many})=>({
