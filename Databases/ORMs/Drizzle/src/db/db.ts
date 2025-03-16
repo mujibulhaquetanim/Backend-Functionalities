411469ffa16cf07981e15738f7e6edd51c675a6a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-16 22:41:50 +0600

    `pass schema` to drizzle connection function

--- Databases/ORMs/Drizzle/src/db/db.ts
+++ Databases/ORMs/Drizzle/src/db/db.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
+import * as schema from "./schema/schema"
 
 //If .env is not loaded, process.env.DATABASE_URL will be undefined, and Drizzle will default to localhost:5432, causing ECONNREFUSED.
 dotenv.config();
@@ -14,4 +15,6 @@ export const connection = postgres(process.env.DATABASE_URL, {
   ssl: 'require'
 });
 
-export const database = drizzle(connection);
+// Passing schema in drizzle() is a best practice as it improves type safety, query efficiency, and overall developer experience.
+// multiple schemas can be passed using schema: {...schema1, ...schema2}
+export const database = drizzle(connection, {schema: {...schema}, logger: true});
