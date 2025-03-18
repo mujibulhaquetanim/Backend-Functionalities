4c1824c1036b5f9871ed4a0653fe4b18f301e6f8
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-18 23:20:31 +0600

    fetch all friends of a user by querying bi-directional relationship of the user and merge them to get the all user

--- Databases/ORMs/Prisma/src/index.ts
+++ Databases/ORMs/Prisma/src/index.ts
@@ -48,23 +48,48 @@ async function main(){
     //     },
     //   });
 
-    const usersWithDetails = await prisma.user.findMany({
-        include: {
-          friendships: {
-            include: {
-              user: true, // Include friend details
-            },
-          },
-          jobTitle: true, // Include job title details
-          profile: true, // Include profile details
-          posts: true, // Include posts details
-        },
-      });
+    // const usersWithDetails = await prisma.user.findMany({
+    //   take: 1, // Limit to 1 user
+    //     include: {
+    //       friendships: {
+    //         include: {
+    //           user: true, // Include friend details
+    //         },
+    //       },
+    //       jobTitle: true, // Include job title details
+    //       profile: true, // Include profile details
+    //       posts: true, // Include posts details
+    //     },
+    //   });
     
-      console.dir(usersWithDetails, { depth: null });
+    //   console.dir(usersWithDetails, { depth: null });
     
     //   console.dir(usersWithFriendships, { depth: null });
 
+    // retrieve user with all friends
+    const userWithAllFriends = await prisma.user.findMany({
+      where: {id: '1134f861-2ec7-4496-bece-79f29785bb85'},
+      include: {
+        friendships: {include: {friend: true}},
+        friends: {include: {user: true}}
+      }
+    })
+    console.dir(userWithAllFriends, {depth: null})
+
+    if(userWithAllFriends.length === 0){
+      console.log("User not found");
+      return
+    }
+
+    //after querying individual friends and friendships data of a user we merge them to get all friends
+    //[0] because we only have one user
+    const allFriends = [
+      ...userWithAllFriends[0].friendships.map(friendship => friendship.friend.name),
+      ...userWithAllFriends[0].friends.map(friend => friend.user.name)
+    ]
+
+    console.log(`allFriends count: ${allFriends.length}`);
+    console.dir(allFriends, {depth: null})
 }
 
 main().then(async ()=>await prisma.$disconnect()).catch(async (error)=> {
