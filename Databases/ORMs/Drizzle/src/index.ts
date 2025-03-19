e0001ee2941c97ef5a99660c36458288a5f5f737
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-19 23:42:47 +0600

    `experiment` with `select` statement of drizzle + `log` userIds after extracting them from the array of objects that returned by the subQuery

--- Databases/ORMs/Drizzle/src/index.ts
+++ Databases/ORMs/Drizzle/src/index.ts
@@ -30,6 +30,8 @@ async function getUsersPost() {
     //The subquery selects distinct authorId values from the posts table by grouping the rows based on authorId. It limits the result to a maximum of 2 rows.
     // const subQuery = database.select({userId: posts.authorId}).from(posts).groupBy(posts.authorId).limit(2);
 
+    //passing a column to select doesn't conform selectedField types of drizzle orm. select expects a object like structure.
+    // const subQuery = await database.select(posts.authorId).from(posts).groupBy(posts.authorId).limit(2).execute();
     const subQuery = await database.select({userId: posts.authorId}).from(posts).groupBy(posts.authorId).limit(2).execute();
     
     // using string literal calls toString() method to this array of objects and returns a string. The default .toString() implementation for non-primitive objects like arrays or complex query objects returns [object Object].This happens because the object isn't being serialized or explicitly converted into a human-readable format.
@@ -38,7 +40,10 @@ async function getUsersPost() {
     //Using string literals (${}) works fine for primitive types (numbers, strings, etc.), but for complex objects like arrays or query results, direct logging or JSON serialization is necessary for meaningful and human-readable output.
     console.log(`subQuery: ${JSON.stringify(subQuery)}`);
 
+    //extract the userId from the subQuery
     const userIds = (await subQuery).map((row)=> row.userId);
+
+    console.log(userIds)
 
     // for multiple users or Array of users use inArray instead of `eq` which is for single user.
     const result = await database.select().from(users).where(inArray(users.id, userIds));
