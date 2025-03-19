18ca3a3c2b342aa755d1dc0ed9a9378d5160bc0a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-19 23:51:47 +0600

    clean the `index file` of drizzle-orm & move the additional infos to a separate `.md` file

--- Databases/ORMs/Drizzle/src/index.ts
+++ Databases/ORMs/Drizzle/src/index.ts
@@ -24,20 +24,8 @@ import { users } from "./db/schema/users.schema"
 // post.then(data => console.log(data)).catch(error => console.error(error));
 
 async function getUsersPost() {
-    // The combined query essentially retrieves user details for a subset of authors (limited to 2) who have posts in the posts table. This query fetches all rows from the users table where the id matches any of the authorIds returned by the subquery
-
-    // it is returned multiple rows of authorId
-    //The subquery selects distinct authorId values from the posts table by grouping the rows based on authorId. It limits the result to a maximum of 2 rows.
-    // const subQuery = database.select({userId: posts.authorId}).from(posts).groupBy(posts.authorId).limit(2);
-
-    //passing a column to select doesn't conform selectedField types of drizzle orm. select expects a object like structure.
-    // const subQuery = await database.select(posts.authorId).from(posts).groupBy(posts.authorId).limit(2).execute();
     const subQuery = await database.select({userId: posts.authorId}).from(posts).groupBy(posts.authorId).limit(2).execute();
     
-    // using string literal calls toString() method to this array of objects and returns a string. The default .toString() implementation for non-primitive objects like arrays or complex query objects returns [object Object].This happens because the object isn't being serialized or explicitly converted into a human-readable format.
-    console.log(`subQuery: ${subQuery}`);
-    // using JSON.stringify() formats the object into a JSON string.
-    //Using string literals (${}) works fine for primitive types (numbers, strings, etc.), but for complex objects like arrays or query results, direct logging or JSON serialization is necessary for meaningful and human-readable output.
     console.log(`subQuery: ${JSON.stringify(subQuery)}`);
 
     //extract the userId from the subQuery
---
+++ Databases/ORMs/Drizzle/src/infos.md
@@ -0,0 +1,28 @@
+# Query Infos:
+
+## getUserPosts()
+
+### what is function does:
+
+    // The combined query essentially retrieves user details for a subset of authors (limited to 2) who have posts in the posts table. This query fetches all rows from the users table where the id matches any of the authorIds returned by the subquery
+
+    // it is returned multiple rows of authorId
+    //The subquery selects distinct authorId values from the posts table by grouping the rows based on authorId. It limits the result to a maximum of 2 rows.
+
+### why passing column name in the select statement in not a valid signature in drizzle orm
+
+    // const subQuery = database.select({userId: posts.authorId}).from(posts).groupBy(posts.authorId).limit(2);
+
+    //passing a column to select doesn't conform selectedField types of drizzle orm. select expects a object like structure.
+    // const subQuery = await database.select(posts.authorId).from(posts).groupBy(posts.authorId).limit(2).execute();
+
+### why using string literal not a good idea
+
+    // using string literal calls toString() method to this array of objects and returns a string. The default .toString() implementation for non-primitive objects like arrays or complex query objects returns [object Object].This happens because the object isn't being serialized or explicitly converted into a human-readable format.
+    console.log(`subQuery: ${subQuery}`);
+
+### what does JSON.stringify does
+
+    // using JSON.stringify() formats the object into a JSON string.
+    //Using string literals (${}) works fine for primitive types (numbers, strings, etc.), but for complex objects like arrays or query results, direct logging or JSON serialization is necessary for meaningful and human-readable output.
+
