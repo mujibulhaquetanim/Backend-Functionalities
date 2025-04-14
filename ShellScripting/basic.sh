842361056615bb1246a776f18c03a892e1d062ad
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-14 23:52:50 +0600

    practice `Array` & `constant` variable in `bash`

--- ShellScripting/basic.sh
+++ ShellScripting/basic.sh
@@ -9,3 +9,11 @@ feature="beauty"
 # variable to store output of command
 hostname=$(hostname)
 echo "i am $hostname, My Crush is $name and she is natural $feature"
+# constant variable
+readonly rating=10
+echo "She is my crush, i rate her $rating/10 😊"
+
+# Use of arrays
+# declare -a names=("Emma" "Emma" "Emma")
+listOfCrushes=("EmmaWatsom" "SabilaNur" "ShraddhaKapoor")
+echo "My Crushes are ${listOfCrushes[0]}, ${listOfCrushes[1]}, ${listOfCrushes[2]}"
