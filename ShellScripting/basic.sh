d86177fe49d4562fe210b008e4468f8f714742df
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-14 23:47:06 +0600

    use of `variable` to store output of a command or storing something...

--- ShellScripting/basic.sh
+++ ShellScripting/basic.sh
@@ -2,3 +2,10 @@
 
 # we can run this using `bash basic.sh` or making it executable using `chmod +x basic.sh` then run `./basic.sh`
 echo "Hello From Bash Script"
+
+# Use of variables
+name="Emma Watson"
+feature="beauty"
+# variable to store output of command
+hostname=$(hostname)
+echo "i am $hostname, My Crush is $name and she is natural $feature"
