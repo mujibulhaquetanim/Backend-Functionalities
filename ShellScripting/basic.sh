afc4949a1b5b55367e92137f6e11a5f67285f38c
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-15 00:49:07 +0600

    `string` operations in bash shell

--- ShellScripting/basic.sh
+++ ShellScripting/basic.sh
@@ -36,3 +36,18 @@ echo "Best feature of each crush is: perfectGirl ${crushesDetails["perfectGirl"]
 # another way of declaring an array
 crushesDetails=(["perfectGirl"]=${listOfCrushes[0]} ["hottest"]=${listOfCrushes[1]} ["bestLook"]=${listOfCrushes[2]})
 echo "Best feature of each crush is: perfectGirl ${crushesDetails["perfectGirl"]} 😍, hottest ${crushesDetails["hottest"]} 🔥, bestLook ${crushesDetails["bestLook"]} 😘"
+
+# string operations:
+# length of the string, # is used for number of characters
+echo "Length of the string is ${#name}"
+# uppercase of the string using ^^
+echo "Uppercase of the string is ${name^^}"
+# lowercase of the string using ,,
+echo "Lowercase of the string is ${name,,}"
+# substring of the string, from index 5 to index 8
+echo "Substring of the string is ${name:5:3}"
+# replace substring of the string
+echo "We can get another beauty if we replace the Last Name: ${name/Watson/Myers}"
+<<comment
+replace substring of the string, first one is variable name and second one is what we want to replace and third one is what we want to replace it with
+comment
