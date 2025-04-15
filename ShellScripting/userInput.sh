b22f476696476e0d14da16b9931efcba36e53b6d
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-15 23:22:06 +0600

    create a file for taking user input using `read` or taking input and `-p` for the prompt

---
+++ ShellScripting/userInput.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+# read is a command that is used to take input from the user and store it in a var, here crushName is the var and -p is for prompt.
+read -p "Enter your Crush Name: " crushName
+
+echo "So your crush is $crushName"
