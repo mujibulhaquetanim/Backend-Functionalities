c9ec6d56fc0e0e9781820275bfc72690775f9c6a
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-16 23:46:37 +0600

    make multiple if-else statement with `elif`

--- ShellScripting/conditionalStatements.sh
+++ ShellScripting/conditionalStatements.sh
@@ -1,8 +1,12 @@
 #!bin/bash
 
-read -p "Guess my crush name: " crushName
+read -p "Guess any of my crush name: " crushName
 
 if [[ $crushName == "Emma Watson" ]];then
+    echo "You are goddamn right"
+elif [[ $crushName == "Sabila Nur" ]];then
+    echo "You are goddamn right"
+elif [[ $crushName == "Shraddha Kapoor" ]];then
     echo "You are goddamn right"
 else
     echo "You are wrong"
