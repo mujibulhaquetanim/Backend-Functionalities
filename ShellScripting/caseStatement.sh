db5ebef27d00e6e1c80ebac1d4e667f7c60eeb4d
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-17 23:26:23 +0600

    [bug fix] in case statement script

--- ShellScripting/caseStatement.sh
+++ ShellScripting/caseStatement.sh
@@ -3,17 +3,17 @@
 echo "---Find Beautiful Woman---"
 echo "Provide option:"
 echo "A for England "
-echo "B for India" "
+echo "B for India"
 echo "C for Bangladesh"
 
 read option
 
 case $option in
-    H) echo "Emma Watson is from England"
+    A) echo "Emma Watson is from England"
     ;;
     B) echo "Shraddha Kapoor is from India"
     ;;
-    D) echo "Sabila Nur is from Bangladesh"
+    C) echo "Sabila Nur is from Bangladesh"
     ;;
     *) echo "Invalid Option"
     ;;
