f9ea996e71498650c70c5e4263d4d9834d518e18
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-18 23:51:25 +0600

    print list of crushes using braces expression

--- ShellScripting/lool.sh
+++ ShellScripting/lool.sh
@@ -1,13 +1,23 @@
 #!bin/bash
 
+# manual loop
 
+# manual loop using string
 for crushName in "Emma Watson" "Sabila Nur" "Shraddha Kapoor"
 do
     echo "My Crush is $crushName"
 done
 
+# manual loop using number
 echo "Number of My Current Crushes"
 for i in 1 2 3 4
 do
     echo "My Crush is $i"
+done
+
+# brace expansion
+listOfCrushes=("Emma Watson" "Sabila Nur" "Shraddha Kapoor")
+for i in {0..2}
+do
+    echo "My Crush is ${listOfCrushes[$i]}"
 done
