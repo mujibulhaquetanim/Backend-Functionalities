44e97143f7cf7f61da2aeff08354995b2ae2e513
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-18 22:54:00 +0600

    add initial script to display crush names and count using loop

---
+++ ShellScripting/lool.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+
+for crushName in "Emma Watson" "Sabila Nur" "Shraddha Kapoor"
+do
+    echo "My Crush is $crushName"
+done
+
+echo "Number of My Current Crushes"
+for i in 1 2 3 4
+do
+    echo "My Crush is $i"
+done
