863d2aa0f3bb892cffe43d8c5637294f94469635
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-19 23:59:55 +0600

    read file line by line and display crush names using a for loop

--- ShellScripting/useCases/readFileUsingLoop/readFileContentUsingLoop.sh
+++ ShellScripting/useCases/readFileUsingLoop/readFileContentUsingLoop.sh
@@ -8,3 +8,8 @@ touch "$FILE"
 
 # Write to the file
 echo -e "Emma Watson\nShraddha Kapoor\nSabila Nur" > "$FILE"
+
+# Read the file line by line using for loop
+for line in $(cat $FILE); do
+    echo "My Crush Name is: $line"
+done
