6b9a1a8461ce0790ee267b8d66b0df70aac0328a
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-21 23:53:12 +0600

    exclude the header of `csv` file + fix formatting and improve comments in readFromCSVfileUsingWhileLoop.sh

--- ShellScripting/useCases/readFileUsingLoop/readFromCSVfileUsingWhileLoop.sh
+++ ShellScripting/useCases/readFileUsingLoop/readFromCSVfileUsingWhileLoop.sh
@@ -1,18 +1,24 @@
-#!bin/bash
+#!/bin/bash
 
 FILE="${PWD}/readFile.csv"
 
-# make csv file
-touch $FILE
+# Make CSV file
+touch "$FILE"
 
-# write to csv file
-echo -e "Name, Age, Country" > $FILE
-echo -e "Emma Watson, 25, England" >> $FILE
-echo -e "Sabila Nur, 30, Bangladesh" >> $FILE
-echo -e "Shraddha Kapoor, 33, India" >> $FILE
+# Write to CSV file
+echo -e "Name, Age, Country" > "$FILE"
+echo -e "Emma Watson, 25, England" >> "$FILE"
+echo -e "Sabila Nur, 30, Bangladesh" >> "$FILE"
+echo -e "Shraddha Kapoor, 33, India" >> "$FILE"
 
-# read from csv file
-while IFS="," read -r name age country
-do
+# Read from CSV file (including the header)
+echo "Reading all lines, including the header:"
+while IFS="," read -r name age country; do
     echo "Name: $name, Age: $age, Country: $country"
 done < "$FILE"
+
+# Read from CSV file (excluding the header)
+echo -e "\nReading all lines, excluding the header:"
+cat "$FILE" | awk 'NR!=1 {print}' | while IFS="," read -r name age country; do
+    echo "Name: $name, Age: $age, Country: $country"
+done
