1706ddd68f38b8e24060684c178b1459d1ba4834
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-22 22:48:35 +0600

    add addition function to functions.sh for basic arithmetic operations

--- ShellScripting/functions.sh
+++ ShellScripting/functions.sh
@@ -7,3 +7,15 @@ function printCrushName(){
 
 # Calling the function and passing arguments
 printCrushName "Emma" "Sabila" "Shraddha" "LaLisa"
+
+# addition of two numbers
+function addition(){
+    # local keyword is used to declare local variables
+    local num1=$1
+    local num2=$2
+    local sum=$((num1 + num2))
+    #return keyword does not work, use echo
+    echo $sum
+}
+result=$(addition 10 20)
+echo "Addition of 10 and 20 is: " $result
