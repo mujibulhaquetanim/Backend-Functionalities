bbfc571f5fa2c525967917d2665976b2309dfb5a
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-22 23:11:19 +0600

    print crushes above 30 years using `awk` pattern matching command + update Emma Watson's age in CSV file and adjust related script output

--- ShellScripting/useCases/readFileUsingLoop/readFromCSVfileUsingWhileLoop.sh
+++ ShellScripting/useCases/readFileUsingLoop/readFromCSVfileUsingWhileLoop.sh
@@ -7,7 +7,7 @@ touch "$FILE"
 
 # Write to CSV file
 echo -e "Name, Age, Country" > "$FILE"
-echo -e "Emma Watson, 25, England" >> "$FILE"
+echo -e "Emma Watson, 35, England" >> "$FILE"
 echo -e "Sabila Nur, 30, Bangladesh" >> "$FILE"
 echo -e "Shraddha Kapoor, 33, India" >> "$FILE"
 
@@ -20,5 +20,11 @@ done < "$FILE"
 # Read from CSV file (excluding the header)
 echo -e "\nReading all lines, excluding the header:"
 cat "$FILE" | awk 'NR!=1 {print}' | while IFS="," read -r name age country; do
+    echo "Name: $name, Age: $age, Country: $country"
+done
+
+# read only person whose age is greater than 30 from csv file
+echo -e "\nReading only person whose age is greater than 30:"
+cat "$FILE" | awk -F "," '$2 > 30 {print}' | while IFS="," read -r name age country; do
     echo "Name: $name, Age: $age, Country: $country"
 done
