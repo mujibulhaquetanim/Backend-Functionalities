2f47fb5474385f26b96e068f4893ddf054646ce4
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-22 22:39:30 +0600

    add functions.sh to define and call printCrushName function

---
+++ ShellScripting/functions.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+function printCrushName(){
+    # $1, $2, $3, $4 are arguments
+    echo "Crush Names: $1 $2 $3 $4"
+}
+
+# Calling the function and passing arguments
+printCrushName "Emma" "Sabila" "Shraddha" "LaLisa"
