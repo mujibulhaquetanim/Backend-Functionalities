0e62a0805c796d0be2e18a857d1fddc8b95ada18
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-23 22:44:54 +0600

    update age of Shraddha Kapoor in readFile.csv and add grep.sh for searching patterns in CSV

---
+++ ShellScripting/grep.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+# grep is a command that is used to search for a pattern in a file and print the lines that contain the pattern.
+# -i is a flag that is used to ignore case sensitivity in the search.
+grep -i "30" "./useCases/readFileUsingLoop/readFile.csv"
