2e8c3b65cb671e3ba246f3346678ab718e6d28ad
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-24 23:20:46 +0600

    `shift` to accept remaining arguments into one variable inside the script

--- ShellScripting/userInput.sh
+++ ShellScripting/userInput.sh
@@ -5,5 +5,10 @@ read -p "Enter your Crush Name: " crushName
 
 echo "So your crush is $crushName"
 
-# arguments are passed to the script using $1, $2, $3, $4 from the command line while executing the script
-echo "Your Crush Name is: $1 $2 $3 $4"
+# arguments are passed to the script using $1 from the command line while executing the script
+echo "Your Crush Name is: $1
+
+# shift keyword is used to shift the arguments to the left, so that the first argument is now the second argument, here after using the first argument rest of the arguments are shifted to variable $2
+# i.e. shript.sh "Emma Watson" the most beautiful girl in the world. here "Emma Watson" is stored in $1 and "the most beautiful girl in the world." is stored in $2
+shift
+echo "Your Crush Name is: $2
