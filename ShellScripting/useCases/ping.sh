badc4784cf01d7e6c7f01445cef5ec121f3cfd0f
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-27 23:31:17 +0600

    remove commented sleep command from `ping` script for clarity and to handle unsuccessful `ping` command

--- ShellScripting/useCases/ping.sh
+++ ShellScripting/useCases/ping.sh
@@ -4,7 +4,8 @@ read -p "Enter IP Address: " ip
 
 # -c is for number of pings and 1 is for one ping
 ping -c 1 $ip
-sleep 3
+# if ping is not successful then if/else will show result for sleep command as $? gives result of last command and it always return 0, that is why removing it by commenting
+# sleep 3
 
 if [[ $? -eq 0 ]]; then
     echo "$ip is up"
