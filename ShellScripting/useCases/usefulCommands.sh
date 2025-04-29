a7355607a0f0b1cf9a88a33e440f743767cd7524
mujibulhaquetanim <mujibultanim@gmail.com>
2025-04-29 23:11:44 +0600

    add additional notes on root password setup to goodToKnow.md and usefulCommands.sh

--- ShellScripting/useCases/usefulCommands.sh
+++ ShellScripting/useCases/usefulCommands.sh
@@ -3,6 +3,7 @@
 # uid of current user, UID of a root user always will be 0, it would be useful to check whether the user is root user or not
 echo "Your UID is: " $UID
 
+# if you're using wsl and haven't set root password while set up, to setup root password, use `sudo passwd root`.
 # example of checking whether the user is root or not
 if [ $UID -eq 0 ]; then
     echo "You are root user"
