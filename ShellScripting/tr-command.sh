ed6643410a055d1f75add85d046acef59a545fe2
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-02 23:59:21 +0600

    add examples for case conversion and file usage in tr-command.sh; create tr-output.txt for output demonstration

--- ShellScripting/tr-command.sh
+++ ShellScripting/tr-command.sh
@@ -28,3 +28,6 @@ echo "Emma Watson" | tr ' ' '\n'
 
 # make lowercase to uppercase and uppercase to lowercase
 echo "Emma Watson" | tr 'a-zA-Z' 'A-Za-z'
+
+# use tr command with files
+tr 'a-zA-Z' 'A-Za-z' < ./useCases/readFileUsingLoop/readFile.txt > tr-output.txt
