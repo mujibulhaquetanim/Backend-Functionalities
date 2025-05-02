1d953b751270bb277887e83a1deee12c018da8bd
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-02 23:42:21 +0600

    update tr-command.sh with additional examples for string manipulation using tr

--- ShellScripting/tr-command.sh
+++ ShellScripting/tr-command.sh
@@ -10,3 +10,12 @@ echo "Emma,Watson" | tr ',' ' '
 
 # delete string also delete any letter that in the string like a in the watson.
 echo "Emma Watson" | tr -d 'Emma'
+
+# make string lowercase
+echo "emma watson" | tr 'a-z' 'A-Z'
+
+# compress multiple repeated spaces or letters.
+echo "Emma Watson!!!" | tr -s 'a  !'
+
+# convert spaces into new line
+echo "Emma Watson" | tr ' ' '\n'
