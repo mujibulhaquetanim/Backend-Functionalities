2cea1677ad6adb9035d96db84f2efdb55cebf678
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-03 22:59:35 +0600

    update sed-command.sh to add echo statements for better output clarity and organization

--- ShellScripting/sed-command.sh
+++ ShellScripting/sed-command.sh
@@ -1,10 +1,22 @@
 #!bin/bash
 
 # read a specific line (1st line) from a file
+echo "---read a specific line (1st line) from a file---"
 sed -n '1p' "./useCases/readFileUsingLoop/readFile.txt"
 
 # read the last line from a file
+echo -e "\n---read the last line from a file---"
 sed -n '$p' "./useCases/readFileUsingLoop/readFile.txt"
 
 # show line in a given range
+echo -e "\n---show line in a given range (2nd and 3rd line)---"
 sed -n '2,3p' "./useCases/readFileUsingLoop/readFile.txt"
+
+# read certain word from a file
+echo -e "\n---read certain word from a file---"
+sed -n '/Bangladesh/p' "./useCases/readFileUsingLoop/readFile.csv"
+
+# replace a word in a file
+echo -e "\n---replace a word in a file---"
+sed 's/Bangladesh/Chattogram/' "./useCases/readFileUsingLoop/readFile.csv"
+
