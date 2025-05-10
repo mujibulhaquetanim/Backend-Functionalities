cd57a6f5669efec130d20ec956ec593a3484c27b
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-10 23:09:59 +0600

    add functionality to find and move files to a new directory, and delete temporary files in find.sh

--- ShellScripting/find.sh
+++ ShellScripting/find.sh
@@ -15,3 +15,13 @@ find . -type f \( -name "*.txt" -o -name "*.log" \)
 # search by file size and greater than 1kb or 1024 bytes, for kb use k, for bytes use c
 echo -e "\n---search by file size---"
 find . -type f -size +1024c
+
+# find and mv to new directory, `{}` acts as a placeholder for each found file. `mv {}` moves the found file to `./files` and `\;` marks the end of the command.
+echo -e "\n---find and mv to new directory---"
+# find . -type f -name "*.txt" -exec mv {} ./files \; # didn't excute the command in terminal
+
+# find and delete files
+echo -e "\n---find and delete files---"
+find /home/haq -type f -name "*.tmp"
+# another way it will show prompt, The -i flag prompts before deleting each file.
+find /home/haq -type f -name "*.tmp" -exec rm -i {} \;
