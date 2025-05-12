1ec00f528ee000dfc2634a0e27ffeccc0af4f975
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-12 23:51:38 +0600

    add interactive options for weather updates in weatherUpdate.sh

--- ShellScripting/useCases/weatherUpdate.sh
+++ ShellScripting/useCases/weatherUpdate.sh
@@ -1,3 +1,17 @@
 #!bin/bash
 
-curl "https://wttr.in/?format=3"
+echo "---Welcome to Weather Update---"
+echo -e "\nFor simpler weather update, press 1"
+echo "For advanced weather update, press 2"
+
+read option
+
+if [[ $option -eq 1 ]]; then
+    curl "https://wttr.in/?format=3"
+elif [[ $option -eq 2 ]]; then
+    echo "Enter the city name: "
+    read city
+    curl "https://wttr.in/$city"
+else
+    echo "Invalid option"
+fi
