b953959542abab016728e7d32fd008b6dee94a28
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-13 23:31:41 +0600

    add types for res, req and fix `/api` endpoint by removing extra res header at the beginning of the switch block

--- Modules/http/createNodeServer.ts
+++ Modules/http/createNodeServer.ts
@@ -1,21 +1,22 @@
-import http from "node:http";
+import http, { IncomingMessage, ServerResponse } from "node:http";
 
-const server = http.createServer((req, res) => {
-    //writeHead is used to set the response header, which is needed to set the content type
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    switch(req.url){
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+    switch (req.url) {
         case "/":
+            res.writeHead(200, { "Content-Type": "text/plain" });
             res.end("Welcome to Node Server");
             break;
         case "/api":
-            res.end(JSON.stringify({message: "Welcome to API"}));
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Welcome to API" }));
             break;
         default:
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("Not Found");
+            break;
     }
 });
 
 server.listen(3000, () => {
-    console.log("listening on port 'http://localhost:3000'");
+    console.log("Listening on port 'http://localhost:3000'");
 });
