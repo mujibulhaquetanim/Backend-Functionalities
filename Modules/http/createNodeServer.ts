80558d52d3383c66635a1b5cb7357a916073b456
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-13 23:44:42 +0600

    refactor server creation to use async/await and parse URL for routing

--- Modules/http/createNodeServer.ts
+++ Modules/http/createNodeServer.ts
@@ -1,7 +1,26 @@
-import http, { IncomingMessage, ServerResponse } from "node:http";
+import http, { IncomingMessage, Server, ServerResponse } from "node:http";
+import url from "node:url";
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    switch (req.url) {
+// const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+//     switch (req.url) {
+//         case "/":
+//             res.writeHead(200, { "Content-Type": "text/plain" });
+//             res.end("Welcome to Node Server");
+//             break;
+//         case "/api":
+//             res.writeHead(200, { "Content-Type": "application/json" });
+//             res.end(JSON.stringify({ message: "Welcome to API" }));
+//             break;
+//         default:
+//             res.writeHead(404, { "Content-Type": "text/plain" });
+//             res.end("Not Found");
+//             break;
+//     }
+// });
+
+const server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    const {query, pathname} = url.parse(req.url || "", true);
+    switch (pathname) {
         case "/":
             res.writeHead(200, { "Content-Type": "text/plain" });
             res.end("Welcome to Node Server");
@@ -15,7 +34,7 @@ const server = http.createServer((req: IncomingMessage, res: ServerResponse) => 
             res.end("Not Found");
             break;
     }
-});
+})
 
 server.listen(3000, () => {
     console.log("Listening on port 'http://localhost:3000'");
