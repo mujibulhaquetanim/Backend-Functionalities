07b10111ed7c44b5667ba3268caea1a53ffa262b
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-13 23:58:30 +0600

    remove deprecated `url.parse()` and use `URL` for it.

--- Modules/http/createNodeServer.ts
+++ Modules/http/createNodeServer.ts
@@ -1,5 +1,5 @@
 import http, { IncomingMessage, Server, ServerResponse } from "node:http";
-import url from "node:url";
+import { URL} from "node:url";
 
 // const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 //     switch (req.url) {
@@ -19,7 +19,19 @@ import url from "node:url";
 // });
 
 const server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
-    const {query, pathname} = url.parse(req.url || "", true);
+    // const {query, pathname} = url.parse(req.url || "", true); // deprecated
+    if(!req.url){
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad Request: No URL");
+        return;
+    }
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+    // Object.fromEntries() Transforms query parameters into an object. The URL.searchParams returns a URLSearchParams instance, which is iterable but not a plain object. Using Object.fromEntries(), we easily convert it into a standard JavaScript object. without this, we have to manually extract each key-value pair from the URLSearchParams instance. i.e. url.searchParams.get(key), url.searchParams.get(age), url.searchParams.get(gender) or manually loop through the URLSearchParams instance using url.searchParams.forEach((value, key) => {})
+    const query = Object.fromEntries(parsedUrl.searchParams);
+    const pathname = parsedUrl.pathname;
+
+    console.log(`query: ${JSON.stringify(query)} | pathname: ${pathname}`);
+
     switch (pathname) {
         case "/":
             res.writeHead(200, { "Content-Type": "text/plain" });
