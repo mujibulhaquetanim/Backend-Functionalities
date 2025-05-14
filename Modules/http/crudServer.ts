d0202a715699d86950e4824f4ec0514001a7fa02
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-14 22:32:17 +0600

    create new file for `crud` & add root endpoint by checking `GET` method & pathName.

---
+++ Modules/http/crudServer.ts
@@ -0,0 +1,29 @@
+import http, {ServerResponse, IncomingMessage} from 'node:http';
+import { URL } from 'node:url';
+
+// helper function to send response
+const sendResponse = (res:ServerResponse ,statusCode: number, message: object | string, contentType: string= "application/json" )=>{
+    res.statusCode = statusCode;
+    res.setHeader("Content-type", contentType);
+    res.end(contentType === 'application/json' ? JSON.stringify(message): message);
+}
+
+const app = http.createServer(async(req: IncomingMessage, res: ServerResponse)=>{
+    if(!req.url){
+        sendResponse(res, 400, {error: "No URL found"})
+        return;
+    }
+    const parsedUrl = new URL(req.url, `http://${req.headers.host || 'http://localhost'}`);
+    const pathname = parsedUrl.pathname;
+    const method = req.method;
+
+    // GET method
+    if(method === 'GET' && pathname=== '/'){
+        sendResponse(res, 200, "Welcome to Node-CRUD")
+        return;
+    }
+})
+
+app.listen(3001, ()=>{
+    console.log(`Listening on Port: 3001 on https://localhost:3001`)
+})
