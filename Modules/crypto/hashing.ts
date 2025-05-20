a6d07ea44178094120ddfb6680917b0d6b87240d
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-20 23:12:50 +0600

    add comments to clarify hash and HMAC creation and comparison processes

--- Modules/crypto/hashing.ts
+++ Modules/crypto/hashing.ts
@@ -2,6 +2,12 @@ import crypto from 'node:crypto';
 
 console.log(crypto.randomBytes(8).toString('hex')); // generate random bytes in buffer and convert to hex
 
+/*
+- createHash() creates a hash object with the specified algorithm
+- update() updates the hash object with the provided data string i.e. 'secret'
+- digest() finalizes the hash and returns the hash value in the specified format i.e. hex
+*/
+
 // creating hash
 const hash = crypto.createHash('sha256');
 console.log(hash) //returns hash object
@@ -18,3 +24,18 @@ console.log(hashFinal) //returns hash in hex
 const hash1 = crypto.createHash('sha256').update('Sabila Nur').digest('hex');
 const hash2 = crypto.createHash('sha256').update('Sabila Nur').digest('hex');
 console.log(hash1 === hash2); //returns true
+
+//-----------------------------------------------------------------------------------------
+
+// hmac is a type of hash used for message authentication
+const hmac = crypto.createHmac('sha256', 'secret');
+console.log(hmac) //returns hmac object
+const hmacUpdate = hmac.update('Sabila Nur');
+console.log(hmacUpdate) //returns hmac object of provided data string
+const hmacFinal = hmacUpdate.digest('hex'); // digest returns buffer of hash in hex
+console.log(hmacFinal) //returns hash in hex
+
+// comparing hmacs
+const hmac1 = crypto.createHmac('sha256', 'secret').update('Sabila Nur').digest('hex');
+const hmac2 = crypto.createHmac('sha256', 'secret').update('Sabila Nur').digest('hex');
+console.log(hmac1 === hmac2); //returns true
