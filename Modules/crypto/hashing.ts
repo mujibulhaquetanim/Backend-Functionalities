4467a258bd114ba819ac3a2bf32829d9591ecb82
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-20 23:07:55 +0600

    refactor hashing module to enhance clarity and organization of hash generation and comparison

--- Modules/crypto/hashing.ts
+++ Modules/crypto/hashing.ts
@@ -1,3 +1,20 @@
 import crypto from 'node:crypto';
 
 console.log(crypto.randomBytes(8).toString('hex')); // generate random bytes in buffer and convert to hex
+
+// creating hash
+const hash = crypto.createHash('sha256');
+console.log(hash) //returns hash object
+
+// updating hash
+const hashUpdate =hash.update('Sabila Nur');
+console.log(hashUpdate) //returns hash object of provided data string
+
+// finalizing hash
+const hashFinal = hashUpdate.digest('hex'); // digest returns buffer of hash in hex
+console.log(hashFinal) //returns hash in hex
+
+// comparing hashes
+const hash1 = crypto.createHash('sha256').update('Sabila Nur').digest('hex');
+const hash2 = crypto.createHash('sha256').update('Sabila Nur').digest('hex');
+console.log(hash1 === hash2); //returns true
