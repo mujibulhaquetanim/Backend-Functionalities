a0d0c668ecfe96a60f7a7370404a61f4b9dcc3bc
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-22 21:11:52 +0600

    enhance scrypto module with asynchronous and synchronous versions, improving security against timing attacks

--- Modules/crypto/scrypto.ts
+++ Modules/crypto/scrypto.ts
@@ -20,3 +20,41 @@ const randomString = crypto.randomBytes(16).toString('hex');
 const scrypto3 = crypto.scryptSync("password".normalize(), randomString.normalize(), 64);
 const scrypto4 = crypto.scryptSync("password".normalize(), randomString.normalize(), 64);
 console.log(scrypto3.toString('hex') === scrypto4.toString('hex')); //returns true
+
+// timing based attacks can be used to break scrypts, to mitigate this, we can use timingSafeEqual() to compare the hashes
+
+// asynchronous version of scrypto
+const scrypto7 = new Promise<Buffer>((resolve, reject) => {
+    crypto.scrypt("password".normalize(), randomString.normalize(), 64, (err, derivedKey) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(derivedKey);
+    });
+});
+
+const scrypto8 = new Promise<Buffer>((resolve, reject) => {
+    crypto.scrypt("password".normalize(), randomString.normalize(), 64, (err, derivedKey) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(derivedKey);
+    });
+});
+
+// Wait for both promises to resolve
+Promise.all([scrypto7, scrypto8])
+    .then(([derivedKey1, derivedKey2]) => {
+        console.log(crypto.timingSafeEqual(
+            Buffer.from(derivedKey1), 
+            Buffer.from(derivedKey2)
+        )); // Should return true
+    })
+    .catch(error => console.error("Error:", error));
+
+// synchronous version of scrypto
+const scrypto5 = crypto.scryptSync("password".normalize(), randomString.normalize(), 64);
+const scrypto6 = crypto.scryptSync("password".normalize(), randomString.normalize(), 64);
+console.log(crypto.timingSafeEqual(Buffer.from(scrypto5,), Buffer.from(scrypto6))); //returns true
