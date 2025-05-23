20a7492feae0991ca8f22ff782b42d9a2533fcff
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-23 21:47:46 +0600

    add success and failure messages to package removal script

--- ShellScripting/useCases/removeInstalledPkgsWithSamePattern.sh
+++ ShellScripting/useCases/removeInstalledPkgsWithSamePattern.sh
@@ -7,4 +7,9 @@ read -p "Enter the pattern to remove: " pattern
 # all of the packages with the pattern @nextui-org/ will be removed
 pnpm list --depth=0 | grep $pattern | awk '{print $1}' | xargs pnpm remove
 
-echo "All packages with the pattern $pattern have been removed."
+# $? is the exit status of the previous command if it is 0 then it is success and if it is 1 then it is failure
+if [ $? -eq 0 ]; then
+    echo "Packages removed successfully."
+else
+    echo "Failed to remove packages."
+fi
