8df8c7dd2c5e06873755d63555c169df08ad6a63
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-31 23:38:41 +0600

    refactor client management in realTimeTCPchat module to use a single clients array

--- Modules/http/realTimeTCPchat.ts
+++ Modules/http/realTimeTCPchat.ts
@@ -1,19 +1,18 @@
 import net from "node:net";
 
+const clients: net.Socket[] = [];
+
 const server = net.createServer((socket) => {
     socket.write("Welcome to the real time TCP chatroom\n");
 
-    const client: net.Socket[] = [];
 
-    socket.on("connect", () => {
-        console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connected`);
-        console.log(`Client count: ${client.length}`);
-        client.push(socket);
-    });
+    console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connected`);
+    console.log(`Client count: ${clients.length}`);
+    clients.push(socket);
 
     socket.on("data", (data) => {
         // send data to all connected clients with their IP and port
-        client.forEach((client) => {
+        clients.forEach((client) => {
             // don't send data back to the sender
             if(client !== socket){
                 client.write(`${socket.remoteAddress}:${socket.remotePort}: ${data}\n\n`);
@@ -23,8 +22,14 @@ const server = net.createServer((socket) => {
 
     socket.on("close", () => {
         console.log(`Client ${socket.remoteAddress}:${socket.remotePort} disconnected`);
-        // remove the client from the list and close the connection
-        client.splice(client.indexOf(socket), 1);
+        // remove the clients from the list and close the connection
+        clients.splice(clients.indexOf(socket), 1);
     });
 
-}).listen(3000);
+    socket.on("error", (error) => {
+        console.error(`Client ${socket.remoteAddress}:${socket.remotePort} error: ${error}`);
+    });
+
+}).listen(3000, () => {
+    console.log(`Server listening on http://localhost:3000`);
+});
