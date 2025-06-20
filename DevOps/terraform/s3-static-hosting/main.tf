d8871310974b5bbf0af6cd0d366bd0ee2fbd4022
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-20 23:01:17 +0600

    add Terraform configuration for S3 static hosting with random bucket name generation and show output of it

---
+++ DevOps/terraform/s3-static-hosting/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+# Generate a random id
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+# using generated random id to create bucket
+resource "aws_s3_bucket" "terraBucket" {
+  bucket = "demo-bucket-${random_id.rand_id.hex}"
+}
+
+# uploading file
+resource "aws_s3_object" "terraData" {
+  bucket = aws_s3_bucket.terraBucket.bucket
+  source = "./index.html"
+  key    = "index.html"
+}
