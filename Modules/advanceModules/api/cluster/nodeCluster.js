9a973e6dcb156dd880df8cebbae654c517f5b3ed
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-24 23:42:02 +0600

    refactor cluster management: enhance logging for primary and worker processes

--- Modules/advanceModules/api/cluster/nodeCluster.js
+++ Modules/advanceModules/api/cluster/nodeCluster.js
@@ -2,12 +2,22 @@ const cluster = require('node:cluster');
 const os = require('os');
 
 const numCPUs = os.cpus().length;
-console.log('numCPUs: ', numCPUs);
 
-if(cluster.isPrimary){
-    for(let i=0;i<numCPUs;i++){
+if (cluster.isPrimary) {
+    console.log(`Primary process ${process.pid} is running`);
+    console.log(`Forking ${numCPUs} workers...\n`);
+
+    for (let i = 0; i < numCPUs; i++) {
+        //cluster.fork(), it spawns a new Node.js worker process.
         cluster.fork();
     }
-}else{
-    console.log(process.pid);
+
+    // Event listener for when a worker exits. it will restart the worker if needed or monitor the cluster
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} exited (code: ${code}, signal: ${signal})`);
+        // Optionally, restart the worker
+        // cluster.fork();
+    });
+} else {
+    console.log(`Worker ${process.pid} started`);
 }
