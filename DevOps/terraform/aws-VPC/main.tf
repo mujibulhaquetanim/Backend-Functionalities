4e8ac23ae90d7b511df4734521eec58b1525bac7
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-25 23:37:45 +0600

    refactor Terraform configuration: add route table and associate with public subnet

--- DevOps/terraform/aws-VPC/main.tf
+++ DevOps/terraform/aws-VPC/main.tf
@@ -50,5 +50,20 @@ resource "aws_internet_gateway" "my-igw" {
   }
 }
 
+# Create a route table
+resource "aws_route_table" "my-rt" {
+    vpc_id = aws_vpc.my-vpc.id
 
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my-igw.id
+    }
+  
+}
+
+# Associate the route table with the public subnet
+resource "aws_route_table_association" "public-rt-association" {
+  route_table_id = aws_route_table.my-rt.id
+  subnet_id = aws_subnet.public-subnet.id
+}
 
