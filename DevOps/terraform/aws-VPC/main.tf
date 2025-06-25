cbd5d1c2dc8f7649e963a7a685200fbfc316b033
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-25 23:25:48 +0600

    add initial Terraform configuration for AWS VPC and subnets

---
+++ DevOps/terraform/aws-VPC/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta3"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "public-subnet" {
+  cidr_block              = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.my-vpc.id
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create a private subnet
+resource "aws_subnet" "private-subnet" {
+  cidr_block              = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.my-vpc.id
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
