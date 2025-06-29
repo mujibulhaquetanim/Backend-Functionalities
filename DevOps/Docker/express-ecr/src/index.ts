3c1f6bccab92ed4cc1db86a58447eacc58c8bbad
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-29 22:52:06 +0600

    add initial project files including package.json, pnpm-lock.yaml, and TypeScript configuration

---
+++ DevOps/Docker/express-ecr/src/index.ts
@@ -0,0 +1,39 @@
+import http, { IncomingMessage, ServerResponse } from "node:http";
+import { URL} from "node:url";
+
+// helper function to send response
+function sendResponse(res: ServerResponse, statusCode: number, message: string | object) {
+    res.statusCode = statusCode;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(JSON.stringify(message));
+}
+
+const server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    // const {query, pathname} = url.parse(req.url || "", true); // deprecated
+    if(!req.url){
+        sendResponse(res, 400, "Bad Request: No URL");
+        return;
+    }
+    // fix: handle missing host in URL parsing for server requests
+    const parsedUrl = new URL(req.url, `http://${req.headers.host || "localhost"}`);
+    const query = Object.fromEntries(parsedUrl.searchParams);
+    const pathname = parsedUrl.pathname;
+
+    console.log(`query: ${JSON.stringify(query)} | pathname: ${pathname}`);
+
+    switch (pathname) {
+        case "/":
+            sendResponse(res, 200, "Welcome to Node Server from ECR");
+            break;
+        case "/health":
+            sendResponse(res, 200, { message: "ECR is healthy" });
+            break;
+        default:
+            sendResponse(res, 404, "Not Found");
+            break;
+    }
+})
+
+server.listen(3000, () => {
+    console.log("Listening on port 'http://localhost:3000'");
+});
