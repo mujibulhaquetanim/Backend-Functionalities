eac591eae130867123daebc4960f062bab1e4411
mujibulhaquetanim <mujibultanim@gmail.com>
2025-07-17 23:37:40 +0600

    refactor(index.ts): remove port binding to support Lambda integration

--- DevOps/Docker/lambda-image/src/app.ts
+++ DevOps/Docker/lambda-image/src/app.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Hello from Lambda Image!');
 });
 
 app.get('/health', (_: Request, res: Response) => {
-  res.sendStatus(200);
+  res.json({ status: 'ok', time: new Date().toISOString() });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+export default app;
