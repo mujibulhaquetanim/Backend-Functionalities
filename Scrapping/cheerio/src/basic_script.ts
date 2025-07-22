6ed2031912a655cd0a3c6e087f9ed54877b35407
mujibulhaquetanim <mujibultanim@gmail.com>
2025-07-22 23:57:18 +0600

    write a bare-bone script for scrapping with `cheerio`

---
+++ Scrapping/cheerio/src/basic_script.ts
@@ -0,0 +1,8 @@
+import * as cheerio from 'cheerio';
+const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+
+$('h2.title').text('Hello there!');
+$('h2').addClass('welcome');
+
+$.html();
+//=> <html><head></head><body><h2 class="title welcome">Hello there!</h2></body></html>
