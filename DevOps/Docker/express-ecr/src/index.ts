087bea6fee544733f7420a51ff1911664e655b19
mujibulhaquetanim <mujibultanim@gmail.com>
2025-07-23 15:56:15 +0600

    enhance Dockerfile and index.ts: add `curl` for health checks and adjust formatting in `index` file

--- DevOps/Docker/express-ecr/src/index.ts
+++ DevOps/Docker/express-ecr/src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
+    import express from "express";
 
-const app = express();
+    const app = express();
 
-app.use(express.json());
+    app.use(express.json());
 
-app.get("/", (_: express.Request, res: express.Response) => {
-    res.send("Hello from Express ECR!");
-});
+    app.get("/", (_: express.Request, res: express.Response) => {
+        res.send("Hello from Express ECR!");
+    });
 
-app.get("/health", (_: express.Request, res: express.Response) => {
-    res.json({ status: "ok", time: new Date().toISOString() });
-}); 
+    app.get("/health", (_: express.Request, res: express.Response) => {
+        res.status(200).json({ status: "ok", time: new Date().toISOString() });
+    }); 
 
-app.listen(8000, () => {
-    console.log("Server is running on http://localhost:8000");
-});
+    app.listen(8000, "0.0.0.0", () => {
+    console.log("Server is running on http://0.0.0.0:8000");
+    });
