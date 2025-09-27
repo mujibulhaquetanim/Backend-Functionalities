ef993c201e2c361c93543dd44310f37acca6bf08
mujibulhaquetanim <mujibultanim@gmail.com>
2025-09-27 23:21:42 +0600

    Implement code changes to enhance functionality and improve performance

--- Projects/googleSheets-automation/index.ts
+++ Projects/googleSheets-automation/index.ts
@@ -2,26 +2,43 @@ import {google} from 'googleapis';
 import { GoogleAuth } from 'google-auth-library';
 import fs from 'fs/promises'
 
-const cred = await fs.readFile('./cred.json', 'utf-8')
+try {
+    const cred = await fs.readFile('./cred.json', 'utf-8')
+    
+    // jsong -> js object
+    const cred_parsed = JSON.parse(cred)
+    // console.log(cred_parsed)
+    
+    const auth = new GoogleAuth({
+        credentials: cred_parsed,
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    })
+    
+    const sheets = google.sheets({version: 'v4', auth})
+    
+    console.log('starting to update...')
 
-// jsong -> js object
-const cred_parsed = JSON.parse(cred)
-// console.log(cred_parsed)
+    const  setValues = await sheets.spreadsheets.values.update({
+        spreadsheetId: '1CcwyDqhpCZ8OOjF7fT3i9NhvlIPjgdwXgINmPUOeMFk',
+        range: 'Sheet1!A1',
+        valueInputOption: 'raw',
+        requestBody: {
+            values: [
+                ['Hello', 'World']
+            ]
+        }
+    })
 
-const auth = new GoogleAuth({
-    credentials: cred_parsed,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
-})
-
-const sheets = google.sheets({version: 'v4', auth})
-
-sheets.spreadsheets.values.update({
-    spreadsheetId: '1CcwyDqhpCZ8OOjF7fT3i9NhvlIPjgdwXgINmPUOeMFk',
-    range: 'Sheet1!A1',
-    valueInputOption: 'raw',
-    requestBody: {
-        values: [
-            ['Hello', 'World']
-        ]
-    }
-})
+    console.log('successfully updated')
+    console.log(setValues.data)
+    
+    console.log('getting values...')
+    const getValues = await sheets.spreadsheets.values.get({
+        spreadsheetId: '1CcwyDqhpCZ8OOjF7fT3i9NhvlIPjgdwXgINmPUOeMFk',
+        range: 'Sheet1'
+    })
+    
+    console.log(getValues.data.values)
+} catch (error) {
+    console.error(error)
+}
